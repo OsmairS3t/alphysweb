611a289359c615421ede2af89bb2f058b1c2a19a
Osmair <osmair144@gmail.com>
2024-07-15 17:56:30 -0300

    Updated

--- src/app/register/category/formCategory.tsx
+++ src/app/register/category/formCategory.tsx
@@ -1,60 +1,40 @@
 import React from 'react';
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from '@aw/components/ui/button';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@aw/components/ui/form';
 import { Input } from '@aw/components/ui/input';
 import { supabase } from '@aw/lib/database';
 import { ICategory } from '@aw/utils/interface';
 
-const formSchema = z.object({
-  id: z.string(),
-  name: z.string().min(2, {
-    message: "Categoria deve ter ao menos 2 caracteres.",
-  }),
+const schemaCategory = z.object({
+  name: z.string().min(2, 'O nome da categoria deve conter no mínimo 2 caracteres.'),
 })
+type TCategory = z.infer<typeof schemaCategory>
 
 export default function FormCategory({frmCat}:{frmCat:ICategory}) {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema)
-  })
-  
-  async function onUpdate(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  const { register, handleSubmit, formState:{errors} } = useForm<TCategory>({
+    resolver: zodResolver(schemaCategory)
+  });
+
+  const onSubmit: SubmitHandler<TCategory> = async (data) => {
     try {
-      await supabase.from('categories').update({ name: values.name }).eq('id', values.id)
+      await supabase.from('categories').insert({ name: data.name })
       alert('Categoria alterada com sucesso!')
-      form.reset()
-    } catch (error) {
+    }catch (error) {
       console.log(error)
     }
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onUpdate)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Categoria:</FormLabel>
-              <FormControl>
-                <Input 
-                  {...field} 
-                  defaultValue={frmCat.name}
-                  placeholder="Nome da categoria" 
-                />
-              </FormControl>
-              <FormDescription>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Salvar</Button>
-      </form>
-    </Form>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <Input
+        className='mt-4'
+        placeholder="Categoria"
+        defaultValue={frmCat.name}
+        {...register('name')}
+      />
+      <Button type="submit">Atualizar</Button>
+    </form>
   )
 }
